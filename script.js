f202ab4ece277e71f4b162597805a50ad001228d
Akash Das <121249808+AkashDas2003@users.noreply.github.com>
2024-04-04 13:19:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+var timer = 30;
+var score = 0;
+var hitrn = 0;
+function increaseScore() {
+              score += 10;
+              document.querySelector("#scoreval").textContent = score;
+}
+function make_bubble() {
+              var clutter = "";
+              for (var i = 1; i <= 112; i++) {
+                            var rn = Math.floor(Math.random() * 10);
+                            clutter += `<div class="bubble">${rn}</div>`;
+
+              }
+              document.querySelector("#pbtm").innerHTML = clutter;
+}
+function runtimer() {
+              var timerinterval = setInterval(function () {
+                            if (timer > 0) {
+                                          timer--;
+                                          document.querySelector("#timerval").textContent = timer;
+                            }
+
+                            else {
+                                          clearInterval(timerinterval);
+                                          document.querySelector("#pbtm").innerHTML = `<h1>GAME OVER</h1>`;
+                            }
+              }, 1000);
+}
+function getnewhit() 
+{
+              hitrn = Math.floor(Math.random() * 10);
+              document.querySelector("#hitval").textContent = hitrn;
+}
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+              var clickednum = (Number(dets.target.textContent));
+              if (clickednum === hitrn) {
+                            increaseScore();
+                            make_bubble();
+                            getnewhit();
+              }  
+})
+getnewhit();
+runtimer();
+make_bubble();
